e8f94e5b737d2bb1ccc99e264fad91281d7efae1
zhou <2214950522@qq.com>
2026-01-05 13:32:44 +0800

    Initial Commit

---
+++ phtot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phtot
+//
+//  Created by zjs on 2026/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phtot/phtotApp.swift
@@ -0,0 +1,17 @@
+//
+//  phtotApp.swift
+//  phtot
+//
+//  Created by zjs on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct phtotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
